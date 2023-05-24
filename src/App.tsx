a9f0873e55dcf2141706e701db631de663766f81
PatilJatin <jatinp8390@gmail.com>
2023-05-24 09:21:13 +0530

    Number of question changed

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export type AnswerObject = {
   correctAnswer: string;
 };
 
-const TOTAL_QUESTIONS = 3;
+const TOTAL_QUESTIONS = 10;
 
 const App: React.FC = () => {
   const [loading, setLoading] = useState(false);
